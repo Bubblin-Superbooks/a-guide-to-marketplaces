32ff8b5eef8bb4e0f42fddfcbe95258bc871d39f
Marvin Danig <marvin@marvindanig.com>
2018-09-06 09:34:28 -0400

    better image of the dashboard

--- crust/scripts/invoker.js
+++ crust/scripts/invoker.js
@@ -1,4 +1,4 @@
-const options = { duration: 300, peel: true, zoom: true, startPage: 84, length: 60 }
+const options = { duration: 300, peel: true, zoom: true, startPage: 88, length: 60 }
 
 // document.addEventListener('DOMContentLoaded', (event) => { console.log('First') })
 
